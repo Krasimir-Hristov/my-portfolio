3653bccd28b9da7f49a5a94b0b7aedc78bb8bf55
krasi <krasimir.xristov@gmail.com>
2023-09-03 23:04:56 +0200

    my build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.VITE_API': JSON.stringify(process.env.VITE_API),
-    'process.env.VITE_SERVER': JSON.stringify(process.env.VITE_SERVER),
-    'process.env.VITE_TEMPLATE': JSON.stringify(process.env.VITE_TEMPLATE)
-  }
 })
