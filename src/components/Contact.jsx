28b1dac1a7edb2a9eda44926bc32b25c9babb6d2
krasi <krasimir.xristov@gmail.com>
2023-10-26 19:49:59 +0200

    Change the send email guncionality with template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,64 +7,8 @@ import { EarthCanvas } from "./canvas";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { slideIn } from "../utils/motion";
 
-//A995jRowqQaGHRgpQ
-//template_x7zp6ra
-//service_zjasm6m
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setLoading(true);
-
-    emailjs
-      .send(
-        "service_zjasm6m",
-        "template_x7zp6ra",
-        {
-          from_name: form.name,
-          to_name: "Krasimir",
-          from_email: form.email,
-          to_email: "krasimir.xristov@gmail.com",
-          message: form.message,
-        },
-        "A995jRowqQaGHRgpQ"
-      )
-      .then(() => {
-        setLoading(false);
-        alert("Thank you. I will get back to you as soon as possible.");
-
-        setForm(
-          {
-            name: "",
-            email: "",
-            message: "",
-          },
-          (error) => {
-            setLoading(false);
-
-            console.log(error);
-
-            alert("Something went wrong.");
-          }
-        );
-      });
-  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -75,52 +19,27 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
+        <div
           className="mt-12 flex flex-col gap-8"
         >
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
-            <input
-              type="text"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows="7"
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What's do you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+          <div className="flex flex-col">
+            <span className="text-white font-medium mb-4">My Name</span>
+            <p className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium">Krasimir Hristov</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white font-medium mb-4">My Address</span>
+            <p className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium">Wndlingen am Neckar - Germany</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white font-medium mb-4">My Email</span>
+            <p className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium">krasimir.xristov@gmail.com</p>
+          </div>
+          <div className="flex flex-col">
+            <span className="text-white font-medium mb-4">My Mobile</span>
+            <p className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium">+49 173 3079213</p>
+          </div>
 
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+        </div>
       </motion.div>
 
       <motion.div
